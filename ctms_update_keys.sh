b1fac44505635514a962c55e98064148b40c8a33
U-PP2\ecooper <PP2+ecooper@PP2.ctmsohio.com.local>
2018-05-10 10:44:51 -0400

    Added yum/apt support Fixed copy force bug

--- ctms_update_keys.sh
+++ ctms_update_keys.sh
@@ -5,28 +5,29 @@ ROOT_AUTH_FILE="/root/.ssh/authorized_keys"
 
 
 apt-get update
-apt-get -y install git patch
+python -mplatform | grep -qi Ubuntu && sudo apt-get update || sudo yum update
+python -mplatform | grep -qi Ubuntu && sudo apt-get -y install git patch || sudo yum -y install git patch
 
 pushd /root
 mkdir -p CTMS
 pushd CTMS
-git clone https://github.com/CTMS/ssh.git 
+git clone https://github.com/CTMS/ssh.git
 cd ssh
-git pull 
+git pull
 
 if [ -e $ROOT_AUTH_FILE ]; then
    diff -u /root/.ssh/authorized_keys pub_key_list.txt > authorized_keys_$DATE.patch
-else 
-   cp pub_key_list.txt $ROOT_AUTH_FILE 
+else
+   cp pub_key_list.txt $ROOT_AUTH_FILE
 fi
 
 patch $ROOT_AUTH_FILE authorized_keys_$DATE.patch
 
 chown root.root $ROOT_AUTH_FILE
 chmod 600 $ROOT_AUTH_FILE
- 
 
-cp ctms_update_keys.sh /etc/cron.daily/
+
+cp -f ctms_update_keys.sh /etc/cron.daily/
 chown root:root /etc/cron.daily/ctms_update_keys.sh
 chmod a+x /etc/cron.daily/ctms_update_keys.sh
 
